ba98d64440797e1efa89e871a3ae31d4389b834b
zhaoyiran924 <32808735+zhaoyiran924@users.noreply.github.com>
2019-05-13 21:35:37 +0800

    Update get_handwork.py

--- get_handwork.py
+++ get_handwork.py
@@ -1,3 +1,5 @@
+#This code is a crawler to get manual shop on www.dianping.com
+
 # -*- coding: utf-8 -*-
 import requests
 import json
@@ -6,7 +8,6 @@ import os
 import re
 import codecs
 from urllib.parse import urlencode
-
 from selenium import webdriver
 
 def get_page_index(use_url):
@@ -20,12 +21,10 @@ def parse_page_index(html):
     if data and 'data' in data.keys():
         for item in data.get('data'):
             yield item.get('article_url')
-
     headers = {
     "Referer" : "https://m.dianping.com/shop/121859722",
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
     }
-
 
 def main():
     #url = "https://m.dianping.com/isoapi/module"
@@ -39,14 +38,10 @@ def do_what_need():
     "Referer" : "https://m.dianping.com/shop/121859722",
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
     }
-
     url = "https://baymax-api.dianping.com/midasmkt/ajax/shopAdsMob?cityId=1&userId=0&viewShopId=121859722&shopType=30&lat=0&lng=0&t=1553182399661&_=1553182399665&callback=Zepto1553182399658"
-
     response = requests.get(url,headers=headers)
     json_str = response.content.decode()
     ret = json.loads(json_str)
-
-
     with open("handwork.txt","w",encoding="utf-8") as f:
          f.write(json.dumps(ret,ensure_ascii=False,indent=2))
 
@@ -54,7 +49,6 @@ def get_write_hand():
     f = open("handwork.txt")
     hand_work  = []
     price_handwork = ['260','298','158','108','78','99','188','95','364','116']
-
     for i in range(10):
         str = f.readline()
         p = r'[\u4e00-\u9fa5]+\w+|[\u4e00-\u9fa5]+\s'
@@ -70,11 +64,7 @@ def get_page(url):
     response = requests.get(url,data=query_string,headers=headers)
     response.encoding = response.apparent_encoding
     return response.text
-
     page = get_page(url)
     f = codecs.open('handwork_html.txt','w','utf-8')
     f.write(page)
-    #print('handwork_html.txt finish')
-
-# if __name__=='__main__':
-#     print(get_write_hand())
+ 
