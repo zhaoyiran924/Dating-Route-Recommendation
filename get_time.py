60828e358c93a0b2b5717fdfe1a00dd855691464
zhaoyiran924 <32808735+zhaoyiran924@users.noreply.github.com>
2019-05-13 22:10:09 +0800

    Update get_time.py

--- get_time.py
+++ get_time.py
@@ -1,8 +1,8 @@
+#This code get time use pc
+
 import time
 
 def get_time():
     time.localtime(time.time())
-
     now_time = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-    
     return now_time
